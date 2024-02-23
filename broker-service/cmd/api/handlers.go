a3154b967310922731292e9364cbde70284d6279
andrelmm91 <110781476+andrelmm91@users.noreply.github.com>
2024-02-23 14:50:55 +0100

    helpers JSON write,read,error

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -5,11 +5,6 @@ import (
 	"net/http"
 )
 
-type jsonResponse struct {
-	Error bool `json:"error"`
-	Message string `json:"message"`
-	Data any `json:"data,omitempty"`
-}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
---
+++ broker-service/cmd/api/helpers.go
@@ -0,0 +1,72 @@
+package main
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error bool `json:"error"`
+	Message string `json:"message"`
+	Data any `json:"data,omitempty"`
+}
+
+func (App *config) readJSON(w http.ResponseWriter, r *http.Request, data any) error{
+	maxBytes := 1048576; // 1Mb
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(Data)
+
+	if err != nil {
+		return err
+	} 
+
+	err := dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return err.New("Body must have a single JSON value")
+	}
+
+	return nil
+}
+
+
+func (App *config) writeJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error{
+	out, err := json.Marshel(data)
+
+	if err != nil {
+		return err
+	} 
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	_, err := w.Write(out)
+
+	if err != nil {
+		return err
+	} 
+
+	return nil
+}
+
+
+func (App *config) errorJSON(w http.ResponseWriter, err error, status ...int) error{
+	statusCode := StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+}
