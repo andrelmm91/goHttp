a4f6fcd836c008ec21b7eae870699b3e2fbef7c8
andrelmm91 <andre.lmm91@gmail.com>
2024-03-02 21:56:49 +0100

    add logger to broler handler

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -10,11 +10,18 @@ import (
 type RequestPayload struct {
 	Action string `json:"action"`
 	Auth AuthPayload `json:"auth,omitempty"`
+	Log LogPayload `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
 	Email string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -39,14 +46,50 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	switch requestPayload.Action {
 	case "auth":
 		app.authenticate(w, requestPayload.Auth)
+	case "log":
+		app.logItem(w, requestPayload.Log)
 	default:
 		app.errorJSON(w, errors.New("unknown action"))
 	}
 }
 
+func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
+	// create some json wee will send to the auth microservice
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+	logServiceURL := "http://logger-service/log"
+
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	// make sure we get back the correct status code
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, err)
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "logged"
+	
+	app.writeJSON(w, http.StatusAccepted, payload)
+}
+
 func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 	// create some json wee will send to the auth microservice
-	jsonData, err := json.MarshalIndent(a, "", "\t")
+	jsonData, _ := json.MarshalIndent(a, "", "\t")
 
 	// call the service
 	request, err := http.NewRequest("POST", "http://authentication-service/authenticate", bytes.NewBuffer(jsonData))
