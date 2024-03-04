6d98231b697bcab390ef8a09b1c8c548f01d5fa6
andrelmm91 <andre.lmm91@gmail.com>
2024-03-04 21:05:41 +0100

    create sendMail and Mailer struct

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Config struct {
+	Mailer Mail
 }
 
 const webPort = "80"
 
 func main() {
-	app := Config{}
+	app := Config{
+		Mailer: createMail(),
+	}
 
 	log.Println("starting mail service on port ", webPort)
 
@@ -25,4 +30,20 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func createMail() Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	m := Mail {
+		Domain: os.Getenv("MAIL_DOMAIN"),
+		Host: os.Getenv("MAIL_HOST"),
+		Port: port,
+		Username: os.Getenv("MAIL_USERNAME"),
+		Password: os.Getenv("MAIL_PASSWORD"),
+		Encryption: os.Getenv("MAIL_ENCRYPTION"),
+		FromName: os.Getenv("MAIL_NAME"),
+		FromAddress: os.Getenv("MAIL_ADDRESS"),
+	}
+
+	return m
 }
