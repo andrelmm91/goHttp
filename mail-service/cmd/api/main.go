1d6e03f02669fd61ba43b42fa134ea600ccf823c
andrelmm91 <andre.lmm91@gmail.com>
2024-03-04 11:34:15 +0100

    setting up mailhog

---
+++ mail-service/cmd/api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Config struct {
+}
+
+const webPort = "80"
+
+func main() {
+	app := Config{}
+
+	log.Println("starting mail service on port ", webPort)
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+	
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
