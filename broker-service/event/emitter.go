4df0680f9e9541c17df1e61b7f199388067406c0
andrelmm91 <andre.lmm91@gmail.com>
2024-03-08 14:37:26 +0100

    rabbitmq in broker service

--- broker-service/event/emitter.go
+++ broker-service/event/emitter.go
@@ -19,7 +19,7 @@ func (e *Emitter) setup() error {
 	return declareExchange(channel)
 }
 
-func (e *Emitter) push(event string, severity string) error {
+func (e *Emitter) Push(event string, severity string) error {
 	channel, err := e.connection.Channel()
 	if err != nil {
 		return err
@@ -29,7 +29,7 @@ func (e *Emitter) push(event string, severity string) error {
 	log.Println("Pushing to channel")
 
 	err = channel.Publish(
-		"log_topic",
+		"logs_topic",
 		severity,
 		false,
 		false,
